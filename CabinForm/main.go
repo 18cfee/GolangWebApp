c534ecc08ff8420c639ee787841593bcba47b3e2
Carl Fee <15cfee@gmail.com>
2019-05-13 19:03:45 -0600

    commentOutDbStuff

--- CabinForm/main.go
+++ CabinForm/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"GolangWebApp/CabinForm/handlers"
 	"context"
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -146,31 +145,31 @@ func main() {
 // 	return db
 // }
 
-func migrate(db *sql.DB) {
-	sql := `
-	CREATE TABLE IF NOT EXISTS polls(
-			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-			name VARCHAR NOT NULL,
-			topic VARCHAR NOT NULL,
-			src VARCHAR NOT NULL,
-			upvotes INTEGER NOT NULL,
-			downvotes INTEGER NOT NULL,
-			UNIQUE(name)
-	);
+// func migrate(db *sql.DB) {
+// 	sql := `
+// 	CREATE TABLE IF NOT EXISTS polls(
+// 			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+// 			name VARCHAR NOT NULL,
+// 			topic VARCHAR NOT NULL,
+// 			src VARCHAR NOT NULL,
+// 			upvotes INTEGER NOT NULL,
+// 			downvotes INTEGER NOT NULL,
+// 			UNIQUE(name)
+// 	);
 
-	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Angular','Awesome Angular', 'https://cdn.colorlib.com/wp/wp-content/uploads/sites/2/angular-logo.png', 1, 0);
+// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Angular','Awesome Angular', 'https://cdn.colorlib.com/wp/wp-content/uploads/sites/2/angular-logo.png', 1, 0);
 
-	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Vue', 'Voguish Vue','https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/400px-Vue.js_Logo.svg.png', 1, 0);
+// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Vue', 'Voguish Vue','https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/400px-Vue.js_Logo.svg.png', 1, 0);
 
-	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('React','Remarkable React','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png', 1, 0);
+// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('React','Remarkable React','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png', 1, 0);
 
-	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Ember','Excellent Ember','https://cdn-images-1.medium.com/max/741/1*9oD6P0dEfPYp3Vkk2UTzCg.png', 1, 0);
+// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Ember','Excellent Ember','https://cdn-images-1.medium.com/max/741/1*9oD6P0dEfPYp3Vkk2UTzCg.png', 1, 0);
 
-	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Knockout','Knightly Knockout','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_1489710848/knockout-js.png', 1, 0);
-`
-	_, err := db.Exec(sql)
+// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Knockout','Knightly Knockout','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_1489710848/knockout-js.png', 1, 0);
+// `
+// 	_, err := db.Exec(sql)
 
-	if err != nil {
-		panic(err)
-	}
-}
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
