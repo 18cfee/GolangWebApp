2bc0afd98445cc400a518825476212172cf0da27
Carl Fee <15cfee@gmail.com>
2019-05-18 21:17:56 -0600

    setting test cookie worked

--- CabinForm/main.go
+++ CabinForm/main.go
@@ -10,7 +10,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 	//_ "github.com/mattn/go-sqlite3"
 )
 
@@ -62,6 +61,8 @@ func startHTTPServer() *http.Server {
 
 	http.HandleFunc("/serveFile", serveFile)
 
+	http.HandleFunc("/testCookie", generateCookie)
+
 	go func() {
 		// returns ErrServerClosed on graceful close
 		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
@@ -74,7 +75,16 @@ func startHTTPServer() *http.Server {
 	}()
 
 	// returning reference so caller can call Shutdown()
+	fmt.Printf("Starting server on Port%v\n", port)
 	return srv
+}
+
+func generateCookie(w http.ResponseWriter, req *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "some value",
+		Path:  "/",
+	})
 }
 
 func serveFile(w http.ResponseWriter, req *http.Request) {
@@ -106,16 +116,6 @@ func main() {
 
 	srv := startHTTPServer()
 
-	var t time.Duration
-
-	t = 2
-
-	fmt.Printf("%T\n", t)
-
-	log.Printf("main: serving for %v seconds", int(t))
-
-	time.Sleep(time.Second * t)
-
 	var anything string
 	fmt.Scanf("%v", &anything)
 
@@ -130,46 +130,3 @@ func main() {
 
 	log.Printf("main: done. exiting")
 }
-
-// func initDB(filepath string) *sql.DB {
-// 	db, err := sql.Open("sqlite3", filepath)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	if db == nil {
-// 		panic("db nil")
-// 	}
-
-// 	return db
-// }
-
-// func migrate(db *sql.DB) {
-// 	sql := `
-// 	CREATE TABLE IF NOT EXISTS polls(
-// 			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-// 			name VARCHAR NOT NULL,
-// 			topic VARCHAR NOT NULL,
-// 			src VARCHAR NOT NULL,
-// 			upvotes INTEGER NOT NULL,
-// 			downvotes INTEGER NOT NULL,
-// 			UNIQUE(name)
-// 	);
-
-// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Angular','Awesome Angular', 'https://cdn.colorlib.com/wp/wp-content/uploads/sites/2/angular-logo.png', 1, 0);
-
-// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Vue', 'Voguish Vue','https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/400px-Vue.js_Logo.svg.png', 1, 0);
-
-// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('React','Remarkable React','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png', 1, 0);
-
-// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Ember','Excellent Ember','https://cdn-images-1.medium.com/max/741/1*9oD6P0dEfPYp3Vkk2UTzCg.png', 1, 0);
-
-// 	INSERT OR IGNORE INTO polls(name, topic, src, upvotes, downvotes) VALUES('Knockout','Knightly Knockout','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_1489710848/knockout-js.png', 1, 0);
-// `
-// 	_, err := db.Exec(sql)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
