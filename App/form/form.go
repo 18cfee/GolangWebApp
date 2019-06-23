ddf1ad7a3d25234d7a38291ddf3eb2c85f102cb9
Carl Fee <15cfee@gmail.com>
2019-06-23 13:20:55 -0600

    frontEndWorkToPassQueryParams

--- App/form/form.go
+++ App/form/form.go
@@ -5,11 +5,15 @@ import (
 	"net/http"
 )
 
+var forms = map[string]map[string]string{}
+
 func Put(w http.ResponseWriter, req *http.Request) {
 	if req.Method != "PUT" {
+		// need some error handling here
 		fmt.Println("the method was ", req.Method)
 	}
 	fmt.Println("the put was called")
+
 }
 
 func Get(w http.ResponseWriter, req *http.Request) {
