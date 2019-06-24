2488f6ff3105eb6b7c3427c26a890a6dbd8934f7
Carl Fee <15cfee@gmail.com>
2019-06-23 22:48:42 -0600

    np

--- App/form/form.go
+++ App/form/form.go
@@ -2,20 +2,25 @@ package form
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-var forms = map[string]map[string]string{}
+// map[formId]json
+var forms = map[string]string{}
 
 func Put(w http.ResponseWriter, req *http.Request) {
 	if req.Method != "PUT" {
 		// need some error handling here
 		fmt.Println("the method was ", req.Method)
 	}
-	fmt.Println("the put was called")
-
+	// get the form from the body
+	formId := req. .Query().Get("formId")
+	req.Body.Close()
+	json, _ := ioutil.ReadAll(req.Body)
+	forms[formId] = string(json)
 }
 
 func Get(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("the get was called")
+	
 }
