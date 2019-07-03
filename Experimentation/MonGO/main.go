997ae719e5237df74b2a140b914afafcd8ae41ef
Carl Fee <15cfee@gmail.com>
2019-07-02 18:40:25 -0600

    runInsertDb

--- Experimentation/MonGO/main.go
+++ Experimentation/MonGO/main.go
@@ -1,67 +1,33 @@
-// package main
-
-// import (
-// 	"context"
-// 	"fmt"
-// 	"log"
-
-// 	"go.mongodb.org/mongo-driver/mongo"
-// 	"go.mongodb.org/mongo-driver/mongo/options"
-// )
-
-// // You will be using this Trainer type later in the program
-// type Trainer struct {
-// 	Name string
-// 	Age  int
-// 	City string
-// }
-
-// func main() {
-// 	// Set client options
-// 	clientOptions := options.Client().ApplyURI("mongodb://cluster0-ydqrv.mongodb.net")
-
-// 	// Connect to MongoDB
-// 	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	// Check the connection
-// 	err = client.Ping(context.TODO(), nil)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Println("Connected to MongoDB!")
-
-// 	ash := Trainer{"Ash", 10, "Pallet Town"}
-
-// 	collection := client.Database("test").Collection("trainers")
-// 	collection.InsertOne(context.TODO(), ash)
-// }
-
 package main
 
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var username = "Carl"
-var host1 = "cluster0-ydqrv.mongodb.net" // of the form foo.mongodb.net
+var username string
+var host1 string // of the form foo.mongodb.net
+
+// You will be using this Trainer type later in the program
+type Trainer struct {
+	Name string
+	Age  int
+	City string
+}
 
 func main() {
 	ctx := context.TODO()
 	pw, ok := os.LookupEnv("mongo_psw")
+	username, ok = os.LookupEnv("mongoUserName")
+	host1, ok = os.LookupEnv("host1")
 
 	if !ok {
-		fmt.Println("error: unable to find MONGO_PW in the environment")
+		fmt.Println("error: unable to find in the environment")
 		os.Exit(1)
 	}
 	mongoURI := fmt.Sprintf("mongodb+srv://%s:%s@%s", username, pw, host1)
@@ -82,4 +48,27 @@ func main() {
 	}
 
 	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("test").Collection("trainers")
+
+	ash := Trainer{"Ash", 10, "Pallet Town"}
+	//misty := Trainer{"Misty", 10, "Cerulean City"}
+	//brock := Trainer{"Brock", 15, "Pewter City"}
+
+	for i := 0; i < 20; i++ {
+		//insertResult, err := collection.InsertOne(context.TODO(), ash)
+		collection.InsertOne(context.TODO(), ash)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	//fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	err = client.Disconnect(context.TODO())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
 }
