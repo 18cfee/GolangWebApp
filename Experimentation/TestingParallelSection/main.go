d926ab95f0a7afc96bfa123bf8cbdf86a2c20f87
Carl Fee <15cfee@gmail.com>
2019-07-04 11:48:11 -0600

    fileConventionParrallExperiments

--- Experimentation/TestingParallelSection/main.go
+++ Experimentation/TestingParallelSection/main.go
@@ -1,15 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var count int
 
 func main() {
+	count = 0
+	var wg sync.WaitGroup
 	fmt.Println("working")
+	wg.Add(1)
 	go func() {
 		fmt.Println("func one")
+		wg.Done()
 	}()
-
+	wg.Add(1)
 	go func() {
 		fmt.Println("func two")
+		wg.Done()
 	}()
-	fmt.Scanln()
+	//fmt.Scanln()
+	var mu sync.Mutex
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go updateCount(&wg, &mu)
+	}
+	wg.Wait()
+	fmt.Println(count)
+}
+
+func updateCount(wg *sync.WaitGroup, mu *sync.Mutex) {
+	for i := 0; i < 100; i++ {
+		mu.Lock()
+		count++
+		mu.Unlock()
+		runtime.Gosched()
+	}
+	wg.Done()
 }
